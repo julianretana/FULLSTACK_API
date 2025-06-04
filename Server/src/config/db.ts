8141db7b1d5212258a0e6631e20055fb290921a4
Julián Retana <julianretana72@gmail.com.com>
2025-06-04 11:04:28 -0600

    Agregado soporte SSL para base de datos en Render

--- Server/src/config/db.ts
+++ Server/src/config/db.ts
@@ -2,12 +2,15 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-
 const db = new Sequelize(process.env.DB_URL!, {
-    models: [__dirname + "/../models/**/*.ts"],
-}
-);
+  models: [__dirname + "/../models/**/*.ts"],
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // para Render, normalmente esto es necesario
+    },
+  },
+});
 
-export default db; 
-
+export default db;
